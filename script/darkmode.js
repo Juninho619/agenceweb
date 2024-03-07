6be5d0fad6afd6aea7f9373411445d5c01562915
Christophe <christophebontemps619@gmail.com>
2024-03-07 16:49:50 +0100

    avancé script du darkmode

--- script/darkmode.js
+++ script/darkmode.js
@@ -1,1 +1,19 @@
-console.log("helloworld");
+const darkModeButton = document.querySelector(".fa-solid");
+function myFunction() {
+  let body = document.querySelector("body");
+  body.classList.toggle("darkbody");
+
+  let header = document.querySelector("header");
+  header.classList.toggle("darkheader");
+
+  let darkStart = document.querySelector(".main-start");
+  darkStart.classList.toggle("main-start-button-dark");
+
+  let darkButtonHeader = document.querySelector("header-up-button");
+  darkButtonHeader = document.querySelector(".header-up-button-dark");
+
+  if (darkModeButton.classList == "fa-solid fa-moon") {
+    darkModeButton.setAttribute("class", "fa-regular fa-sun");
+  } else darkModeButton.setAttribute("class", "fa-solid fa-moon");
+}
+darkModeButton.addEventListener("click", myFunction);
